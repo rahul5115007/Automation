ede064a2cdbe46d64832ed4a2864945c2e37e979
rahul joshi <rahuljoshi5611742@gmail.com>
2026-01-07 10:17:36 +0530

    HelloDialog commit

---
+++ HelloDialog.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class HelloDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(
+                null,
+                "Hello Rahul",
+                "Greeting",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+}
